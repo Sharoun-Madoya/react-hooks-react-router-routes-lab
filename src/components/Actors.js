cf7ed1ebe3383774af48c3c006d8178d2dd5f8cb
[sharoun <[madoyasharoun@gmail.com]>
2023-04-28 20:18:22 +0300

    Complete

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,7 +2,23 @@ import React from "react";
 import { actors } from "../data";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+  const actorsList = actors.map((actor) =>(
+  <div key={actor.name}>
+  <h2>Name: {actor.name}</h2>
+  <p>Movies</p>
+  <ul>
+    {actor.movies.map((movie)=>(
+    <li key={movie}>{movie}</li>
+  ))}
+  </ul>
+  </div> ));
+
+
+  return ( <div>
+    {/*{code here}*/}
+    <h1>Actors Page</h1>
+    {actorsList}
+    </div>);
 }
 
 export default Actors;
